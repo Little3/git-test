f8dafd0a79ccb0a8801ea5498f93fcf927ecd58a
wangyang <1969177938@qq.com>
2020-03-05 12:50:28 +0800

    吧本地工程添加到git仓库

---
+++ git-test-project/src/com/tit/FirstClass.java
@@ -0,0 +1,32 @@
+package com.tit;
+
+/**
+ * <p>
+ * Title:com.tit
+ * </p>
+ * <p/>
+ * <p>
+ * Description: 描述【】
+ * </p>
+ * <p/>
+ * <p>
+ * Copyright: Copyright (c) 2019
+ * </p>
+ * <p/>
+ * <p>
+ * Company: 英才汇硕信息技术有限公司
+ * </p>
+ *
+ * @author wangyang
+ * @version 1.0
+ * @created 2020-03-05 12:30
+ */
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
